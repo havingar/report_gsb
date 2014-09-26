4d72b443844c5004123c86892be90a62981ca8b7
Richard Havinga <r.havinga@ulcc.ac.uk>
2014-09-26 11:08:22 +0100

    Add a Disable to Cron

--- settings.php
+++ settings.php
@@ -51,6 +51,9 @@ if ($ADMIN->fulltree) {
 
     $settings->add(new admin_setting_configcheckbox('gsb/automedal',
         get_string('automedal', 'report_gsb'), get_string('automedalxp', 'report_gsb'), 0));
+
+    $settings->add(new admin_setting_configcheckbox('gsb/crondisable',
+        get_string('crondisable', 'report_gsb'), get_string('crondisablexp', 'report_gsb'), 0));
 		
 	$settings->add(new admin_setting_pickroles('gsb/studentrole', get_string('roles'),
                         '', array('student'))); 	
